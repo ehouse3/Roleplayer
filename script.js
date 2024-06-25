f8ef0f8d20b79a148ff0f0ae66a3fd7e85fdfd79
ehouse3 <54383796+ehouse3@users.noreply.github.com>
2024-06-25 10:39:52 -0400

    movement handler in class format
    can now create token objects using the token class. This will help in the long run of making objects and having more control over the handler and how it works.

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ class Token {
     class to create a movable and storable token objects:
     name: name of the token
     element: html element
-    num_width: number of tiles wide
-    num_height: number of tiles tall
     cur_x: current x coordinate
     cur_y: current y coordinate
 
@@ -20,15 +18,16 @@ class Token {
         this.element = element;
         this.cur_x = cur_x;
         this.cur_y = cur_y;
+        this.set_position(cur_x, cur_y);
+        
         
         this.dragging = null;
-        this.eventToCoordinates = this.eventToSvgCoordinates;
 
         console.log("testing js outputs and functionality :");
         console.log();
 
     }
-    //getters
+    //getters, fix to be in get format
     get_name() {return this.name;}
     get_element() {return this.element;}
     get_width() {return this.num_width;}
@@ -42,6 +41,8 @@ class Token {
         this.cur_x = snapToClosest(clamp(new_x, this.element.getAttribute('r'), 500 - this.element.getAttribute('r')), 12);
         this.cur_y = snapToClosest(clamp(new_y, this.element.getAttribute('r'), 500 - this.element.getAttribute('r')), 12);
 
+        //console.log("cur x : " + this.cur_x + " cur y : " + this.cur_y);
+
         this.element.setAttribute('cx', this.cur_x);
         this.element.setAttribute('cy', this.cur_y);
         
@@ -53,7 +54,7 @@ class Token {
 
         //snaps value, snaps value to closest factor of unit_length if it's lower or higher (closest square)
         function snapToClosest(x, unit_length) {
-            remainder = x % unit_length;
+            var remainder = x % unit_length;
             if(remainder < unit_length/2) {
                 return x - (x % unit_length);
             }else {
@@ -62,43 +63,55 @@ class Token {
         }
     }
 
-    //makeDraggable(state, el);
     
-    makeDraggable() {
+    //https://www.redblobgames.com/making-of/draggable/examples.html
+    //draggability handler
+    make_draggable() {
+        console.log("adding drag");
+        //pass in an instance of the class to fix scope
+        var _this = this;
+
+        function eventToSvgCoordinates(event, el=event.currentTarget) {
+            const svg = el.ownerSVGElement;
+            let p = svg.createSVGPoint();
+            p.x = event.clientX;
+            p.y = event.clientY;
+            p = p.matrixTransform(svg.getScreenCTM().inverse());
+            return p;
+        }
+
         function start(event) {
             if (event.button !== 0) return; // left button only
-            let {x, y} = this.eventToCoordinates(event);
-            this.dragging = {dx: this.cur_x - x, dy: this.cur_y - y};
-            this.element.classList.add('dragging');
-            this.element.setPointerCapture(event.pointerId);
+            console.log(event);
+            let {x, y} = eventToSvgCoordinates(event);
+            _this.dragging = {dx: _this.cur_x - x, dy: _this.cur_y - y};
+            _this.element.classList.add('dragging');
+            _this.element.setPointerCapture(event.pointerId);
         }
 
         function end(_event) {
-            this.dragging = null;
-            this.element.classList.remove('dragging');
+            _this.dragging = null;
+            _this.element.classList.remove('dragging');
         }
 
         function move(event) {
-            if (!this.dragging) return;
-            let {x, y} = this.eventToCoordinates(event);
-            this.set_position(x + this.dragging.dx, y + this.dragging.dy);
+            if (!_this.dragging) return;
+            let {x, y} = eventToSvgCoordinates(event);
+            _this.set_position(x + _this.dragging.dx, y + _this.dragging.dy);
         }
 
         this.element.addEventListener('pointerdown', start);
         this.element.addEventListener('pointerup', end);
         this.element.addEventListener('pointercancel', end);
-        this.element.addEventListener('pointermove', move)
+        this.element.addEventListener('pointermove', move);
         this.element.addEventListener('touchstart', (e) => e.preventDefault());
     }
 
-    eventToSvgCoordinates(event, el=event.currentTarget) {
-        const svg = el.ownerSVGElement;
-        let p = svg.createSVGPoint();
-        p.x = event.clientX;
-        p.y = event.clientY;
-        p = p.matrixTransform(svg.getScreenCTM().inverse());
-        return p;
+    stop_draggable() {
+        console.log("removing draggability from " + this.name);
     }
+
+    
 
 }
 
@@ -117,7 +130,7 @@ function board_button() {
 }
 
 //game board
-let grid_height =500;
+let grid_height = 500;
 let grid_width = 500;
 
 const grid = document.getElementById("grid");
@@ -130,11 +143,21 @@ game_board_svg.setAttribute('height',grid_height);
 game_board_svg.setAttribute('viewBox','0 0 ' + grid_width + ' ' + grid_height);
 
 
-//https://www.redblobgames.com/making-of/draggable/examples.html
-//draggability handler
-const new_element = document.getElementById("token");
-const new_token = new Token("woot",new_element, 50, 50);
-new_token.makeDraggable();
+
+let new_element = document.getElementsByClassName("token")[0]; //getElementsByClassName returns a list
+//let new_element = document.getElementById("token");
+console.log(new_element);
+let new_token = new Token("woot",new_element, 100, 100);
+new_token.make_draggable();
+
+
+let new_element2 = document.getElementsByClassName("token")[1]; //getElementsByClassName returns a list
+//let new_element = document.getElementById("token");
+console.log(new_element2);
+let new_token2 = new Token("woot",new_element2, 200, 200);
+new_token2.make_draggable();
+
+
 
 
 
