147f380f1aa2451801179bbde7b272c3c6b7fe31
ehouse3 <houseesh@gmail.com>
2024-07-03 12:34:37 -0400

    updated comments
    added comments through js file for handlers, sections, and improve overall readability

--- script.js
+++ script.js
@@ -51,26 +51,20 @@ class Token {
         }
         //snaps value, snaps value to closest factor of unit_length if it's lower or higher (closest square)
         function snap_to_grid(x, unit_length, width) {
-            
             var remainder = x % unit_length;
-            if(width % 12 == 0) {
-                console.log("object is even number of tiles");
+            if(width % 12 == 0) { //token is even width
                 if(remainder < unit_length/2) {
                     return x - remainder;
                 }else {
                     return x - remainder + unit_length;
                 }
-            }else {
-                console.log("object is odd number of tiles");
-                if(remainder - unit_length/2 < unit_length/2) {
-                    return x - remainder + unit_length/2;
+            }else { //token is odd width
+                if(remainder + unit_length/2 < unit_length/2) { //unit_length/2 accounts for odd width
+                    return x - remainder - unit_length/2;
                 }else {
-                    return x - remainder + unit_length + unit_length/2;
+                    return x - remainder + unit_length - unit_length/2;
                 } 
             }
-
-
-           
         }
     }
 
@@ -81,7 +75,7 @@ class Token {
         //pass in an instance of the class to fix scope
         var _this = this;
 
-        function eventToSvgCoordinates(event, el=event.currentTarget) {
+        function event_to_svg_coordinates(event, el=event.currentTarget) {//converts event's argument coordinates to svg coordinates
             const svg = el.ownerSVGElement;
             let p = svg.createSVGPoint();
             p.x = event.clientX;
@@ -90,25 +84,26 @@ class Token {
             return p;
         }
 
-        function start_drag(event) {
-            if (event.button !== 0) return; // left button only
+        function start_drag(event) { //starting dragging event handler
+            if (event.button !== 0) return; //on left click
             let {x, y} = eventToSvgCoordinates(event);
             _this.dragging = {dx: _this.cur_x - x, dy: _this.cur_y - y};
             _this.element.classList.add('dragging');
             _this.element.setPointerCapture(event.pointerId);
         }
 
-        function end_drag(_event) {
+        function end_drag(_event) { //ending drag event handler
             _this.dragging = null;
             _this.element.classList.remove('dragging');
         }
 
-        function move_drag(event) {
+        function move_drag(event) { //dragging on mouse move event handler
             if (!_this.dragging) return;
             let {x, y} = eventToSvgCoordinates(event);
             _this.set_position(x + _this.dragging.dx, y + _this.dragging.dy);
         }
 
+        //binding handlers
         this.element.addEventListener('pointerdown', start_drag);
         this.element.addEventListener('pointerup', end_drag);
         this.element.addEventListener('pointercancel', end_drag);
@@ -132,7 +127,7 @@ function board_button() {
 }
 
 
-//game board
+//game board and variables
 let grid_height = 1000;
 let grid_width = 2000;
 
@@ -147,23 +142,19 @@ board_svg.setAttribute('width',grid_width);
 board_svg.setAttribute('height',grid_height);
 board_svg.setAttribute('viewBox','0 0 ' + grid_width + ' ' + grid_height);
 
-//console.log("window width: " + body.offsetWidth);
-//console.log("width " + board_container.offsetWidth);
+//zoom functions
 var cur_board_width = board_container.offsetWidth;
 var cur_board_height = board_container.offsetHeight;
 var cur_zoom_value = 100;
 function set_zoom(new_zoom_value, cursor_x, cursor_y) {
-    //console.log("window width: " + body.offsetWidth);
-    //console.log(cursor_x);
-    //console.log(cursor_y);
-    
-    board_container.style.zoom = new_zoom_value + "%";
+    board_container.style.zoom = new_zoom_value + "%"; //updates client zoom for board
 
-    
-    if(cursor_x == null && cursor_y == null) { //called from not scroll wheel
-        board_container.scrollBy((cur_board_width - board_container.offsetWidth)/2 , (cur_board_height - board_container.offsetHeight)/2);
-    }else if(cursor_x && cursor_y) { //called from scroll wheel
-        board_container.scrollBy((cur_board_width - board_container.offsetWidth)/2 , (cur_board_height - board_container.offsetHeight)/2);//centers
+    //board movement for zoom (scroll wheel and zoom slider)
+    if(cursor_x == null && cursor_y == null) { //zoom slider
+        board_container.scrollBy((cur_board_width - board_container.offsetWidth)/2 , (cur_board_height - board_container.offsetHeight)/2); //centers screen
+    }else if(cursor_x && cursor_y) { //scroll wheel & mouse
+        //translates screen by cursor amount proportional to zoom (the more zoomed in, the less it translates)
+        board_container.scrollBy((cur_board_width - board_container.offsetWidth)/2 , (cur_board_height - board_container.offsetHeight)/2); //centers screen
         
         var cur_centered_cursor_x = ((cursor_x-(body.offsetWidth/2)) / (cur_zoom_value/100));
         var cur_centered_cursor_y = ((cursor_y-(body.offsetHeight/2)) / (cur_zoom_value/100));
@@ -179,15 +170,12 @@ function set_zoom(new_zoom_value, cursor_x, cursor_y) {
     cur_board_height = board_container.offsetHeight;
     cur_zoom_value = new_zoom_value;
 
-    //console.log("width " + board_container.offsetWidth);
-    //console.log("zoom level percent: " + new_zoom_value);
-    console.log("\n");
 }
 
-//detects input from zoom slider then calls set_zoom()
+//zoom slider handler
 var zoom_slider = document.getElementById("zoom_slider");
 zoom_slider.oninput = function() {
-    set_zoom(Number(zoom_slider.value), null, null);
+    set_zoom(Number(zoom_slider.value), null, null); //calls zoom slider
 }
 
 //sets zoom_slider value to +1/-1 step then calls set_zoom() to match the new value
@@ -210,21 +198,21 @@ board_container.addEventListener("wheel", mouse_zoom);
 
 //panning
 board_container.scrollTo(400, 400); //scrolls on start to have board in frame
-var panning = false;
-function start_pan(event) {
+var panning = false;    
+function start_pan(event) { //starting pan event handler
     if(event.button == 2){
         //console.log("starting pan");
         panning = true;   
         event.preventDefault();
     }
 }
-function end_pan(event) {
+function end_pan(event) { //ending pan event handler
     if(event.button == 2){
         //console.log("ending pan");
         panning = false;
     }   
 }
-function move_pan(event) {
+function move_pan(event) { //panning on mouse movement event handler
     if(panning) {    
         board_container.scrollBy(-1 * Number(event.movementX) * (100/zoom_slider.value), -1 * Number(event.movementY) * (100/zoom_slider.value))
     }
@@ -236,26 +224,19 @@ board_svg.addEventListener('pointercancel', end_pan);
 board_svg.addEventListener('pointermove', move_pan);
 board_svg.addEventListener("contextmenu", (event) => event.preventDefault()); //prevent the right click contextmenu from opening on the gameboard
 
-//arrow key bindings    
-function keydown_handler(event) {
-    console.log(event.key);
-}
-document.addEventListener("keydown", keydown_handler);
+
 //creating tokens
-let new_element = document.getElementsByClassName("token")[0]; //getElementsByClassName returns a list
-//let new_element = document.getElementById("token");
+let new_element = document.getElementsByClassName("token")[0]; 
 let new_token = new Token("big token",new_element, 100, 100, 6);
 new_token.make_draggable();
 
 
-let new_element2 = document.getElementsByClassName("token")[1]; //getElementsByClassName returns a list
-//let new_element = document.getElementById("token");
+let new_element2 = document.getElementsByClassName("token")[1]; 
 let new_token2 = new Token("small token",new_element2, 200, 200, 18);
 new_token2.make_draggable();
 
 
-let new_element3 = document.getElementsByClassName("token")[2]; //getElementsByClassName returns a list
-//let new_element = document.getElementById("token");
+let new_element3 = document.getElementsByClassName("token")[2]; 
 let new_token3 = new Token("really large token",new_element3, 400, 400);
 new_token3.make_draggable();
 
