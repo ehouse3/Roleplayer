7a195726e369453b3bb3d37aea34bd81deb9b935
ehouse3 <houseesh@gmail.com>
2024-08-06 13:50:58 -0400

    fixed scroll down delay mistake
    added new variable to scroll down and scroll up delay for ms to wait until next scroll event allowed.

--- Home_Script.js
+++ Home_Script.js
@@ -38,23 +38,23 @@ var filler = document.getElementById('section_2_filler');
 var scroll_list = [0, 460, 800]; //list of each locations
 var scroll_list_i = 0; //itterable
 var scroll_delay_passed = true;
+var delay = 600; //ms
 function scroll_handler(event) {
     if(event.deltaY > 0 && scroll_list_i != scroll_list.length-1 && scroll_delay_passed == true) {
         scroll_delay_passed = false;
         scroll_list_i += 1;
-        console.log("down : " + scroll_list[scroll_list_i]);
+        //console.log("down : " + scroll_list[scroll_list_i]);
         window.scrollTo(0, Number(scroll_list[scroll_list_i]));
-        setTimeout(() => scroll_delay_passed = true, 250);
+        setTimeout(() => scroll_delay_passed = true, delay);
     }else if(event.deltaY < 0 && scroll_list_i != 0 && scroll_delay_passed == true){
         scroll_delay_passed = false
         scroll_list_i -= 1;
-        console.log("up : " + scroll_list[scroll_list_i]);
+        //console.log("up : " + scroll_list[scroll_list_i]);
         window.scrollTo(0, Number(scroll_list[scroll_list_i]));
-        setTimeout(() => scroll_delay_passed = true, 500);
+        setTimeout(() => scroll_delay_passed = true, delay);
     }
 }
 window.addEventListener('wheel', scroll_handler);
-//window.scrollTo(0, 450);
 window.addEventListener("wheel", e => e.preventDefault(), { passive:false })
 
 
